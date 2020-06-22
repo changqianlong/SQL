5589ad35486c20345443cf3a275046756d9eb48c
乾子 <1748867491@qq.com>
2020-06-22 22:37:56 +0800

    第一次提交

---
+++ 代码/视频2.sql
@@ -0,0 +1,27 @@
+select *
+from Student
+where  sage >=18
+and ssex = '男';
+
+-- 查找年龄大于18岁且学院是计算机学院或者性别为男的同学
+SELECT * 
+FROM Student
+WHERE sage > 18 and (sdept = '计算机学院'
+ or ssex = '男'); 
+ 
+SELECT * from Student
+
+select *
+from Student
+where  sage >=18
+and ssex = '男';
+
+-- 查找年龄大于18岁且学院是计算机学院或者性别为男的同学
+SELECT * 
+FROM Student
+WHERE sage > 18 and (sdept = '计算机学院'
+ or ssex = '男'); 
+ 
+SELECT * from Student
+
+
